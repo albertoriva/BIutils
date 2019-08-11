afc8c965a34e63d3fa34b8769f1aee3e0afa30ea
Alberto Riva <ariva@ufl.edu>
2019-08-11 15:37:06 -0400

    small fixes

--- BImisc.py
+++ BImisc.py
@@ -38,6 +38,64 @@ def linkify(url, name, target="_blank"):
         name = os.path.split(url)[1]
     return "<A target='{}' href='{}'>{}</A>".format(target, url, name)
 
+def get_iterator(dict):
+    if PYTHON_VERSION == 2:
+        return dict.iteritems()
+    else:
+        return dict.items()
+
+def genOpen(filename, mode):
+    """Generalized open() function - works on both regular files and .gz files."""
+    (name, ext) = os.path.splitext(filename)
+    if ext == ".gz":
+        return gzip.open(filename, mode)
+    else:
+        return open(filename, mode)
+
+def decodeUnits(x):
+    if x.endswith("G"):
+        return (x[:-1], 1000000000)
+    if x.endswith("M") or x.endswith("m"):
+        return (x[:-1], 1000000)
+    else:
+        return (x, 1)
+    
+def parseFraction(f):
+    """Parse a fraction (a string of the form N/D) returning a float.
+Returns None if f is not in the form N/D, or if D is 0."""
+    p = f.find("/")
+    if p < 1:
+        return None
+    s1 = f[:p]
+    s2 = f[p+1:]
+    try:
+        v1 = int(s1)
+        v2 = int(s2)
+    except ValueError:
+        return None
+    if v2:
+        return 1.0 * v1 / v2
+    else:
+        return None
+
+class Output():
+    destination = sys.stdout
+    out = None                  # stream
+    __doc__ = "A class that returns a stream to an open file, or sys.stdout if the filename is None or '-'."
+
+    def __init__(self, destination):
+        if destination != '-':
+            self.destination = destination
+
+    def __enter__(self):
+        if self.destination:
+            self.out = genOpen(self.destination, "w")
+        return self.out
+
+    def __exit__(self, type, value, traceback):
+        if self.destination:
+            self.out.close()
+
 class ShellScript():
     filename = ""
     out = None
